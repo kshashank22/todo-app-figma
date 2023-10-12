6c4b420b095649de4f2376d01c78deec73cc05e2
kshashank22 <kshashank.rinku@gmail.com>
2023-10-12 16:53:18 +0530

    updated the button components

--- src/Components/TodoListItems/TodoListItems.jsx
+++ src/Components/TodoListItems/TodoListItems.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import "./TodoListItems.css";
 
-const TodoListItems = ({ todoList }) => {
+const TodoListItems = ({ todoList, setTodoList }) => {
+  const updateCheckbox = (eachTodo) => {
+    setTodoList(
+      todoList.map((eachElement) => {
+        if (eachElement.id === eachTodo.id) {
+          return { ...eachElement, status: !eachElement.status };
+        }
+        return eachElement;
+      })
+    );
+  };
+
   return (
     <ul>
       {todoList.map((eachElement) => {
         return (
           <li key={eachElement.id} className="todo-list-items-container">
-            <input type="radio" className="checkboxStyling" />
+            <input
+              type="checkbox"
+              className="checkboxStyling"
+              onChange={() => updateCheckbox(eachElement)}
+              checked={eachElement.status}
+            />
+
             <div className="contentBox">
               <label className="contentStyling">{eachElement.title}</label>
+              {eachElement.status ? (
+                <label className="circle completeStatus"></label>
+              ) : (
+                <label className="circle inCompleteStatus"></label>
+              )}
             </div>
           </li>
         );
--- src/Pages/TodoAppPage/TodoAppPage.jsx
+++ src/Pages/TodoAppPage/TodoAppPage.jsx
@@ -2,33 +2,46 @@ import React, { useState } from "react";
 import "./TodoAppPage.css";
 import TodoInput from "../../Components/TodoInput/TodoInput";
 import TodoListItems from "../../Components/TodoListItems/TodoListItems";
-
+import TodoButton from "../../Components/TodoButton/TodoButton";
 
 function TodoAppPage() {
-  const [display,setDisplay]=useState(false)
+  const [display, setDisplay] = useState(false);
   const [todoInput, setTodoInput] = useState("");
-  const [todoList, setTodoList] = useState([])
+  const [todoList, setTodoList] = useState([]);
 
-
-  const addTodo=()=>{
-    setDisplay(true)
-  }
+  const addTodo = () => {
+    setDisplay(true);
+  };
   return (
     <div className="todoAppPageContainer">
-      <form className="headerContainer">
+      <div className="headerContainer">
         <h1 className="todoHeading">Today</h1>
         <div>
-        <img
-            src="https://res.cloudinary.com/dw7ksdscg/image/upload/v1697090631/More_uvu5pb.png"
-            className="addTodo"
-            alt="add"
-            onClick={addTodo}  
-          /> 
-        </div>    
-      </form>
-      {display &&<TodoInput display={display} setDisplay={setDisplay} todoInput={todoInput} setTodoInput={setTodoInput} todoList={todoList} setTodoList={setTodoList}/> }
-      {todoList.length>0 && (
-        <TodoListItems todoList={todoList}/>
+          <TodoButton
+            onclick={addTodo}
+            className={"addButton"}
+            value={
+              <img
+                src="https://res.cloudinary.com/dw7ksdscg/image/upload/v1697090631/More_uvu5pb.png"
+                className="addTodo"
+                alt="add"
+              />
+            }
+          />
+        </div>
+      </div>
+      {display && (
+        <TodoInput
+          display={display}
+          setDisplay={setDisplay}
+          todoInput={todoInput}
+          setTodoInput={setTodoInput}
+          todoList={todoList}
+          setTodoList={setTodoList}
+        />
+      )}
+      {todoList.length > 0 && (
+        <TodoListItems todoList={todoList} setTodoList={setTodoList} />
       )}
     </div>
   );
